b71b45e2db0b477bbf05f83e58aafc67486457a6
mygorithm2020 <mygorithm2020@gmail.com>
2024-07-04 12:08:41 +0900

    [etc] : 임시 커밋

---
+++ etc/crawling.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from urllib.request import Request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+
+url = "https://apply.lh.or.kr/lhapply/apply/wt/wrtanc/selectWrtancInfo.do"
+req = Request(url,headers={'User-Agent':'Mozila/5.0'})
+webpage = urlopen(req)
+soup = BeautifulSoup(webpage)
+print(soup)
