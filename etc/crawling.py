5157960a70522d8cc0f2c128d2e021939089f95f
mygorithm2020 <mygorithm2020@gmail.com>
2024-10-25 14:51:44 +0900

    ??

--- etc/crawling.py
+++ etc/crawling.py
@@ -3,8 +3,6 @@ from urllib.request import Request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-
-
 url = "https://apply.lh.or.kr/lhapply/apply/wt/wrtanc/selectWrtancInfo.do"
 req = Request(url,headers={'User-Agent':'Mozila/5.0'})
 webpage = urlopen(req)
